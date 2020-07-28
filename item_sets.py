03a6b6a8ece86f4100a96ce29459e0bb1469d8a5
Peter Keenan <keenan@posteo.net>
2020-07-28 13:18:19 +0100

    Minor tody

--- item_sets.py
+++ item_sets.py
@@ -3,19 +3,22 @@ from struct import *
 FILENAME = "PYTHON.MUS"
 f = open(FILENAME, "rb")
 
+
 def file_counter():
-    COUNTER, = unpack("h",f.read(2))
+    (COUNTER,) = unpack("h", f.read(2))
     return COUNTER
+
 
 ITEM_NUMBER = 1
 p_list = []
 
+
 def item_set():
-    ITEM_COUNTER, = unpack('f', f.read(4))
+    (ITEM_COUNTER,) = unpack("f", f.read(4))
     return ITEM_COUNTER
     p_list.append(ITEM_COUNTER)
     if ITEM_COUNTER > 0:
-        p_number, = unpack('f',f.read(4))
+        (p_number,) = unpack("f", f.read(4))
         p_list.append(p_number)
         ITEM_COUNTER -= 1
         return p_number
