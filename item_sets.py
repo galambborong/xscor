7d14963b54b12d6405551390ab8ab6392a77ec18
Peter Keenan <keenan@posteo.net>
2020-07-29 09:47:25 +0100

    Docstrings added to function definitions

--- item_sets.py
+++ item_sets.py
@@ -3,12 +3,31 @@ from struct import *
 FILENAME = "PYTHON.MUS"
 f = open(FILENAME, "rb")
 
+# Trailing commas prevent the value being returned as a tuple.
 
 def short():
+    """
+    Read short, unpack, return.
+
+    C type: short
+    Python type: integer
+    Standard size: 2 bytes
+
+    See https://docs.python.org/3/library/struct.html#format-characters for more info
+    """
     short, = unpack("h", f.read(2))
     return short
 
 def float():
+    """
+    Read float, unpack, return.
+
+    C type: float
+    Python type: float
+    Standard size: 4 bytes
+
+    See https://docs.python.org/3/library/struct.html#format-characters for more info
+    """
     float, = unpack("f",f.read(4))
     return float
 
@@ -17,6 +36,10 @@ def float():
 
 
 def make_item_set():
+    """
+    Create list, create counter. Loop through following data, appending
+    to list, until counter = 0. Print list to screen.
+    """
     p_list = []
 #    ITEM_COUNTER, = float() THIS DOESN'T WORK YOU FOOL
     p_list.append(float())
