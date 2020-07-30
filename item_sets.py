c8db56d2b6a1fdd460ba6d13bbb1ee22890bc647
Peter Keenan <keenan@posteo.net>
2020-07-30 19:49:43 +0100

    Item set enumerated and turned into dict
    This now enumerates each item set into a dict in which the
    SCORE parameter number is the first index of the dictionary.

--- item_sets.py
+++ item_sets.py
@@ -17,7 +17,7 @@ def short():
 
     See https://docs.python.org/3/library/struct.html#format-characters for more info
     """
-    short, = unpack("h", f.read(2))
+    (short,) = unpack("h", f.read(2))
     return short
 
 
@@ -31,39 +31,27 @@ def float():
 
     See https://docs.python.org/3/library/struct.html#format-characters for more info
     """
-    float, = unpack("f",f.read(4))
+    (float,) = unpack("f", f.read(4))
     return float
 
+
 TOTAL_COUNTER = short()
-ITEM_NUMBER = 0
 
 
 def make_item_set():
     """
     Create list, create counter. Loop through following data, appending
-    to list, until counter = 0. Print list to screen.
+    to list, until counter = 0. Enumerate, convert to dictionary whilst 
+    omitting the item_counter, return.
     """
     item_list = []
     item_counter = float()
-#    global TOTAL_COUNTER
-#    TOTAL_COUNTER -= int(item_counter)
     item_list.append(item_counter)
     item_counter = int(item_counter + 1)
-    if item_counter > 0: # and TOTAL_COUNTER > 0:
+    if item_counter > 0:
         for parameter in range(item_counter - 1):
             item_list.append(float())
-    return(item_list)
-#    print(TOTAL_COUNTER)
-    
-# The TOTAL_COUNTER statements are totally ineffective currently
-
-
-
-#def assign_item_number():
-#    if TOTAL_COUNTER > 0:
-#        for i in range(TOTAL_COUNTER - 1):
-#            ITEM_NUMBER += 1
-#        number = str(ITEM_NUMBER)
-#             = make_item_set
-
-    
+    item_set = list(enumerate(item_list))
+    x = item_set[1:-1]
+    p_list = dict(x)
+    return p_list
